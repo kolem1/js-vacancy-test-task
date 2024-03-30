998caf72edcb04171e2ca11cfa41e13cc5ab96ca
Konstantin Lemko <lemko99@bk.ru>
2024-03-30 23:10:21 +0300

    fix: update cookies domain

--- apps/api/src/services/auth/auth.helper.ts
+++ apps/api/src/services/auth/auth.helper.ts
@@ -21,7 +21,7 @@ export const setTokenCookies = ({
 
   ctx.cookies.set(COOKIES.ACCESS_TOKEN, accessToken, {
     httpOnly: true,
-    domain: cookiesDomain,
+    domain: `.${cookiesDomain}`,
     expires: new Date(Date.now() + 10 * 365 * 24 * 60 * 60 * 1000), // 10 years
   });
 };
